c6c116d2fcba745eaf1b2ffa1447532b7d32074d
Samit Kapadia <41078184+Samitkk18@users.noreply.github.com>
2020-05-04 18:58:41 +0530

    Add files via upload

---
+++ credential.php
@@ -0,0 +1,7 @@
+<?php 
+	/*Enter your API_KEY*/
+	define("API_KEY", 'zJPyWWDBBvA-c1YmQDOW7YJVl9hc7xGIL8KqhEIKk0');
+
+	/*You can enter mobile number here*/
+	define("MOBILE", '');
+ ?>
---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Send SMS from PHP using textlocal</title>
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+</head>
+<body>
+<div class="container">
+<h1 class="text-center">Sending OTP SMS in PHP from localhost using textlocal</h1>
+<hr>
+	<div class="row">
+	<div class="col-md-9 col-md-offset-2">
+		<?php
+			if(isset($_POST['sendotp'])) {
+				require('textlocal.class.php');
+				require('credential.php');
+
+				$textlocal = new Textlocal(false, false, API_KEY);
+
+                // You can access MOBILE from credential.php
+				// $numbers = array(MOBILE);
+                // Access enter mobile number in input box
+                $numbers = array($_POST['mobile']);
+
+				$sender = 'TXTLCL';
+				$otp = mt_rand(10000, 99999);
+				$message = "Hello " . $_POST['uname'] . " This is your OTP: " . $otp;
+
+				try {
+				    $result = $textlocal->sendSms($numbers, $message, $sender);
+				    setcookie('otp', $otp);
+				    echo "OTP successfully send..";
+				} catch (Exception $e) {
+				    die('Error: ' . $e->getMessage());
+				}
+			}
+
+			if(isset($_POST['verifyotp'])) { 
+				$otp = $_POST['otp'];
+				if($_COOKIE['otp'] == $otp) {
+					echo "Congratulation, Your mobile is verified.";
+				} else {
+					echo "Please enter correct otp.";
+				}
+			}
+		?>
+	</div>
+    <div class="col-md-9 col-md-offset-2">
+        <form role="form" method="POST" enctype="multipart/form-data">
+            <div class="row">
+                <div class="col-sm-9 form-group">
+                    <label for="uname">Name</label>
+                    <input type="text" class="form-control" id="uname" name="uname" value="" maxlength="10" placeholder="Enter your name" required="">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-9 form-group">
+                    <label for="mobile">Mobile</label>
+                    <input type="text" class="form-control" id="mobile" name="mobile" value="" maxlength="10" placeholder="Enter valid mobile number" required="">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-9 form-group">
+                    <button type="submit" name="sendotp" class="btn btn-lg btn-success btn-block">Send OTP</button>
+                </div>
+            </div>
+            </form>
+            <form method="POST" action="">
+            <div class="row">
+                <div class="col-sm-9 form-group">
+                    <label for="otp">OTP</label>
+                    <input type="text" class="form-control" id="otp" name="otp" placeholder="Enter OTP" maxlength="5" required="">
+                </div>
+            </div>
+             <div class="row">
+                <div class="col-sm-9 form-group">
+                    <button type="submit" name="verifyotp" class="btn btn-lg btn-info btn-block">Verify</button>
+                </div>
+            </div>
+        </form>
+	</div>
+</div>
+</body>
+</html>
